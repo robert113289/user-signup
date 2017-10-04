494d1978875be3782b2b2a11dcfe883409120ca3
Robert Kirkum <robert113289@gmail.com>
2017-10-04 04:29:55 -0500

    almost finished error message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,12 +6,22 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    
+    error = request.args.get("error")
+
+    return render_template('index.html',title="User Sign up",error=error)
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
     username = request.form['username']
-    return render_template('welcome.html',title="Welcome",username=username)
+
+    err = "ON" 
+    if err == "ON":
+        error = "oops the ship has sunk"
+        return redirect("/?error=" + error)
+
+
+    return render_template('welcome.html',title="Welcome",username=username,)
 
 
 app.run()
