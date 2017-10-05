3831fcff2b18aba86633f6c00b475ab2e0f74511
Robert Kirkum <robert113289@gmail.com>
2017-10-04 19:31:32 -0500

    completed validation

--- main.py
+++ main.py
@@ -7,21 +7,69 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     
-    error = request.args.get("error")
+    return render_template('index.html',title="User Sign up")
 
-    return render_template('index.html',title="User Sign up",error=error)
+
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
+
     username = request.form['username']
+    password = request.form['password']
+    password2 = request.form['password2']
+    email = request.form['email']
+    error = ""
+#username validation
+     #username is empty
+    if username == "":
+        error = "You cannot have an empty username"
+        return render_template('index.html',username_error=error,username=username,email=email)
 
-    err = "ON" 
-    if err == "ON":
-        error = "oops the ship has sunk"
-        return redirect("/?error=" + error)
+     #username is not the right length
+    if len(username) < 3 or len(username) > 20:
+        error = "Your username has to be atleast 3 characters long but no more than 20"
+        return render_template('index.html',title="Signup Error",username_error=error,username=username,email=email)
 
+    #username has a space
+    if " " in username:
+        error = "You cannot have a space in your username"
+        return render_template('index.html',title="Signup Error",username_error=error,username=username,email=email)
 
-    return render_template('welcome.html',title="Welcome",username=username,)
+#password validation
+    #password is empty
+    if password == "":
+        error = "You cannot have an empty password"
+        return render_template('index.html',title="Signup Error",password_error=error,username=username,email=email)
+
+    #password is not the right length
+    if len(password) < 3 or len(password) > 20:
+        error = "Your password has to be atleast 3 characters long but no more than 20"
+        return render_template('index.html',title="Signup Error",password_error=error,username=username,email=email)
+
+    #password has a space
+    if " " in password:
+        error = "You cannot have a space in your username"
+        return render_template('index.html',title="Signup Error",password_error=error,username=username,email=email)
+
+    #passwords do not match
+    if password != password2:
+        error = "Your passwords do not match"
+        return render_template('index.html',title="Signup Error",password_error=error,username=username,email=email)
+
+#email validation
+    #email does not have an @
+    if "@" not in email:
+        error = "You do not have a valid email"
+        return render_template('index.html',title="Signup Error",email_error=error,username=username,email=email)
+
+    if "." not in email:
+        error = "You do not have a valid email"
+        return render_template('index.html',title="Signup Error",email_error=error,username=username,email=email)
+
+        
+    
+
+    return render_template('welcome.html',title="Welcome",username=username)
 
 
 app.run()
